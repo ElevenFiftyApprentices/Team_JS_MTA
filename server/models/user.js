135435b13bc45216fee85be2a124b87eaf7f00ff
trevormahoney <trevormahoney12345@gmail.com>
2017-03-10 10:26:19 -0500

    -

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,12 @@
-var mongoose = mongoose.Schema;
-var Schema = mongoose.Schema;
-var userSchema = new Schema({
-	email: {
-		type: String,
-		unique: true,
-		lowercase: true
-	},
-	password: String
-});
-var model = mongoose.model('user', userSchema);
-module.export = model;
+// var mongoose = mongoose.Schema;
+// var Schema = mongoose.Schema;
+// var userSchema = new Schema({
+// 	email: {
+// 		type: String,
+// 		unique: true,
+// 		lowercase: true
+// 	},
+// 	password: String
+// });
+// var model = mongoose.model('user', userSchema);
+// module.export = model;
