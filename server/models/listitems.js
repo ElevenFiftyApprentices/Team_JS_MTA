70c9f97945da45d292149fc498e4b4f93ee69be9
Matt Dickson <mdickson1737@gmail.com>
2017-03-13 13:35:10 -0400

    added model and table

---
+++ server/models/listitems.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ItemsSchema = new Schema({
+	contents: {
+		type: String,
+		default: ''
+	},
+	shoppingListId: {
+		type: Number,
+		default: 0
+	},
+	priority: {
+		type: Number,
+		default: 1
+	},
+	isChecked: {
+		type: Boolean,
+		default: false
+	},
+	specificUser: {
+		type: String,
+		default: ''
+	}
+});
+
+module.exports = mongoose.model('listItem', ItemsSchema);
