476ad1d9ff9dcefd30c30fe4758427988aabc148
Matt Dickson <mdickson1737@gmail.com>
2017-03-15 16:36:38 -0400

    working for bronze almost

--- modules/shoppinglists/client/controllers/shoppinglists.client.controller.js
+++ modules/shoppinglists/client/controllers/shoppinglists.client.controller.js
@@ -20,7 +20,7 @@
     vm.remove = remove;
     vm.addItem = addItem;
     vm.save = save;
-    vm.checkBtn= checkBtn;
+    //vm.checkBtn= checkBtn;
     // vm.listColor = '#000000';
     // Remove existing Shoppinglist
     function remove() {
@@ -29,32 +29,32 @@
       }
     }
 
-    function checkBtn(isValid){
-      if(!vm.shoppinglist.item.isChecked){
-        vm.shoppinglist.item.isChecked = true
-      } else{
-        vm.shoppinglist.item.isChecked = false
-      }
-      if(vm.shoppinglist._id){
-        vm.shoppinglist.$update(successCallback, errorCallback);
-      }
-      if(!isValid){
-        $scope.$broadcast('show-errors-check-validity', 'vm.form.checkItems');
-        return false;
-      }
-      if(vm.shoppinglist._id){
-        vm.shoppinglist.$update(successCallback, errorCallback);
-      }
-      function successCallback(res) {
-        $state.go('shoppinglists.view', {
-          shoppinglistId: res._id
-        });
-      }
+    // function checkBtn(isValid){
+    //   if(!vm.shoppinglist.item.isChecked){
+    //     vm.shoppinglist.item.isChecked = true;
+    //   } else{
+    //     vm.shoppinglist.item.isChecked = false;
+    //   }
+    //   if(vm.shoppinglist._id){
+    //     vm.shoppinglist.$update(successCallback, errorCallback);
+    //   }
+    //   if(!isValid){
+    //     $scope.$broadcast('show-errors-check-validity', 'vm.form.checkItems');
+    //     return false;
+    //   }
+    //   if(vm.shoppinglist._id){
+    //     vm.shoppinglist.$update(successCallback, errorCallback);
+    //   }
+    //   function successCallback(res) {
+    //     $state.go('shoppinglists.view', {
+    //       shoppinglistId: res._id
+    //     });
+    //   }
 
-      function errorCallback(res) {
-        vm.error = res.data.message;
-      }
-    }
+    //   function errorCallback(res) {
+    //     vm.error = res.data.message;
+    //   }
+    // }
     
 
     function addItem(isValid){
